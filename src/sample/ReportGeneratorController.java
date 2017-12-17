5e356bf87a63503deb933e626e88d5934aa26724
Goh Shang Shein <vegeg@DESKTOP-018HC3A>
2017-12-17 20:08:35 +0800

    saasasa

--- src/sample/ReportGeneratorController.java
+++ src/sample/ReportGeneratorController.java
@@ -7,11 +7,15 @@ package sample;
 
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -47,7 +51,7 @@ import javafx.util.Duration;
 public class ReportGeneratorController implements Initializable {
 
     @FXML
-    private ChoiceBox<String> comparisonCombo;
+    private ComboBox<String> comparisonCombo;
     @FXML
     private PieChart pieChart;
     @FXML
@@ -78,110 +82,110 @@ public class ReportGeneratorController implements Initializable {
 
         comparison.setValue("Money");
         
-
-        XYChart.Series<String, Number> asd = new XYChart.Series<String, Number>();
+        datePicker.setValue(LocalDate.now());
         
-        comparisonCombo.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
-                barChart.getData().clear();
-                for (int i = 0; i <= Integer.parseInt(comparisonCombo.getValue()); i++) {
-                    asd.getData().add(generate(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))));
-                }
-                asd.setName("Revenue");
-                barChart.getData().addAll(asd);
-            }
-        });
-
-        comparison.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
-                barChart.getData().clear();
-                for (int i = 0; i <= Integer.parseInt(comparisonCombo.getValue()); i++) {
-                    asd.getData().add(generate(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))));
-                }
-                asd.setName("Revenue");
-                barChart.getData().addAll(asd);
-            }
-        });
-
-        datePicker.valueProperty().addListener(new ChangeListener<LocalDate>() {
-            @Override
-            public void changed(ObservableValue<? extends LocalDate> observable, LocalDate oldValue, LocalDate newValue) {
-                barChart.getData().clear();
-                for (int i = 0; i <= Integer.parseInt(comparisonCombo.getValue()); i++) {
-                    asd.getData().add(generate(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))));
-                }
-                asd.setName("Revenue");
-                barChart.getData().addAll(asd);
-            }
-        });
-
-//        final String austria = "Austria";
-//        final String brazil = "Brazil";
-//        final String france = "France";
-//        final String italy = "Italy";
-//        final String usa = "USA";
-//        xAxis.setLabel("Food");
-//        yAxis.setLabel("Revenue");
-//
-//        XYChart.Series series1 = new XYChart.Series();
-//        series1.setName("2003");
-//        series1.getData().add(new XYChart.Data(austria, 25601.34));
-//        series1.getData().add(new XYChart.Data(brazil, 20148.82));
-//        series1.getData().add(new XYChart.Data(france, 10000));
-//        series1.getData().add(new XYChart.Data(italy, 35407.15));
-//        series1.getData().add(new XYChart.Data(usa, 12000));
-//
-//        XYChart.Series series2 = new XYChart.Series();
-//        series2.setName("2004");
-//        series2.getData().add(new XYChart.Data(austria, 57401.85));
-//        series2.getData().add(new XYChart.Data(brazil, 41941.19));
-//        series2.getData().add(new XYChart.Data(france, 45263.37));
-//        series2.getData().add(new XYChart.Data(italy, 117320.16));
-//        series2.getData().add(new XYChart.Data(usa, 14845.27));
-//
-//        XYChart.Series series3 = new XYChart.Series();
-//        series3.setName("2005");
-//        series3.getData().add(new XYChart.Data(austria, 45000.65));
-//        series3.getData().add(new XYChart.Data(brazil, 44835.76));
-//        series3.getData().add(new XYChart.Data(france, 18722.18));
-//        series3.getData().add(new XYChart.Data(italy, 17557.31));
-//        series3.getData().add(new XYChart.Data(usa, 92633.68));
-//        barChart.getData().addAll(series1, series2, series3);
         barChart.setLegendSide(Side.TOP);
-        ObservableList<PieChart.Data> pieChartData
-                = FXCollections.observableArrayList(
-                        new PieChart.Data("A", 1),
-                        new PieChart.Data("B", 2),
-                        new PieChart.Data("C", 5));
-        pieChart.setData(pieChartData);
-
+        
         // Changing random data after every 1 second.
         Timeline timeline = new Timeline();
         timeline.getKeyFrames().add(new KeyFrame(Duration.millis(1000), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                for (XYChart.Series<String, Number> series : barChart.getData()) {
-                    for (XYChart.Data<String, Number> data : series.getData()) {
-                        Number yValue = data.getYValue();
-                        Number randomValue = yValue.doubleValue() * (0.5 + Math.random());
-                        data.setYValue(randomValue);
-                    }
-                }
-
-                for (PieChart.Data series2 : pieChart.getData()) {
-                    Number yValue = series2.getPieValue();
-                    Number randomValue = yValue.doubleValue() * (0.5 + Math.random());
-                    series2.setPieValue((double) randomValue);
-
-                }
+                
             }
         }));
         // Repeat indefinitely until stop() method is called.
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.setAutoReverse(true);
         timeline.play();
+        
+        XYChart.Series<String, Number> asd = new XYChart.Series<String, Number>();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        barChart.getData().clear();
+        for (int i = 0; i <= Integer.parseInt(comparisonCombo.getValue()); i++) {
+            File f = new File("C:\\Users\\vegeg\\Documents\\NetBeansProjects\\Restaurant-Ordering-System-BRENZ\\src\\sample\\Log-" + java.sql.Date.valueOf(datePicker.getValue().minusDays(i)) + ".txt");
+            if(f.exists() && !f.isDirectory()){
+            asd.getData().add(generate(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))));
+            pieChartData.add(new PieChart.Data(dateFormat.format(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))), generate1(java.sql.Date.valueOf(datePicker.getValue().minusDays(i)))));
+
+            }
+        }
+        asd.setName("Revenue");
+        barChart.getData().addAll(asd);
+
+        pieChart.setData(pieChartData);
+        
+        comparisonCombo.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
+                XYChart.Series<String, Number> asd = new XYChart.Series<String, Number>();
+                ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                
+                barChart.getData().clear();
+                for (int i = 0; i <= Integer.parseInt(comparisonCombo.getValue()); i++) {
+                    File f = new File("C:\\Users\\vegeg\\Documents\\NetBeansProjects\\Restaurant-Ordering-System-BRENZ\\src\\sample\\Log-" + java.sql.Date.valueOf(datePicker.getValue().minusDays(i)) + ".txt");
+                    if(f.exists() && !f.isDirectory()){
+                    asd.getData().add(generate(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))));
+                    pieChartData.add(new PieChart.Data(dateFormat.format(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))), generate1(java.sql.Date.valueOf(datePicker.getValue().minusDays(i)))));
+                   
+                    }
+                }   
+                asd.setName("Revenue");
+                barChart.getData().addAll(asd);
+        
+                pieChart.setData(pieChartData);
+            }
+        });
+
+        comparison.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
+                XYChart.Series<String, Number> asd = new XYChart.Series<String, Number>();
+                ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                
+                barChart.getData().clear();
+                for (int i = 0; i <= Integer.parseInt(comparisonCombo.getValue()); i++) {
+                    File f = new File("C:\\Users\\vegeg\\Documents\\NetBeansProjects\\Restaurant-Ordering-System-BRENZ\\src\\sample\\Log-" + java.sql.Date.valueOf(datePicker.getValue().minusDays(i)) + ".txt");
+                    if(f.exists() && !f.isDirectory()){
+                    asd.getData().add(generate(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))));
+                    pieChartData.add(new PieChart.Data(dateFormat.format(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))), generate1(java.sql.Date.valueOf(datePicker.getValue().minusDays(i)))));
+                   
+                    }
+                }
+                asd.setName("Revenue");
+                barChart.getData().addAll(asd);
+        
+                pieChart.setData(pieChartData);
+            }
+        });
+
+        datePicker.valueProperty().addListener(new ChangeListener<LocalDate>() {
+            @Override
+            public void changed(ObservableValue<? extends LocalDate> observable, LocalDate oldValue, LocalDate newValue) {
+                XYChart.Series<String, Number> asd = new XYChart.Series<String, Number>();
+                ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                
+                barChart.getData().clear();
+                for (int i = 0; i <= Integer.parseInt(comparisonCombo.getValue()); i++) {
+                    File f = new File("C:\\Users\\vegeg\\Documents\\NetBeansProjects\\Restaurant-Ordering-System-BRENZ\\src\\sample\\Log-" + java.sql.Date.valueOf(datePicker.getValue().minusDays(i)) + ".txt");
+                    if(f.exists() && !f.isDirectory()){
+                    asd.getData().add(generate(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))));
+                    pieChartData.add(new PieChart.Data(dateFormat.format(java.sql.Date.valueOf(datePicker.getValue().minusDays(i))), generate1(java.sql.Date.valueOf(datePicker.getValue().minusDays(i)))));
+                   
+                    }
+                }
+                asd.setName("Revenue");
+                barChart.getData().addAll(asd);
+        
+                pieChart.setData(pieChartData);
+            }
+        });
+
     }
 
     public XYChart.Data generate(Date date) {
@@ -212,6 +216,36 @@ public class ReportGeneratorController implements Initializable {
         
         data = new XYChart.Data(date.toString(), TotalPrice);
         return data;
+    }
+    
+    public double generate1(Date date) {
+        XYChart.Data data = new XYChart.Data();
+        BufferedReader read = null;
+        
+        try {
+            read = new BufferedReader(new FileReader("C:\\Users\\vegeg\\Documents\\NetBeansProjects\\Restaurant-Ordering-System-BRENZ\\src\\sample\\Log-" + date.toString() + ".txt"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ReportGeneratorController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        String mainDateRead;
+        String mainDate[] = null;
+
+        try {
+            mainDateRead = read.readLine();
+            mainDate = mainDateRead.split("x");
+        } catch (IOException ex) {
+            Logger.getLogger(ReportGeneratorController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        double TotalPrice = 0;
+
+        for (int i = 0; i < mainDate.length; i = i + 3) {
+            TotalPrice = TotalPrice + new Double(Double.parseDouble(mainDate[i + 1]));
+        }
+        
+        
+        return TotalPrice;
     }
 
 }
